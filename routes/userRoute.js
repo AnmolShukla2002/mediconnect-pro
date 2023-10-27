93195408f006deabb1362204a1380c4ca88b0b25
Anmol Shukla <anmol.28422@gmail.com>
2023-10-28 02:16:39 +0530

    protecting routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,11 +6,13 @@ import {
   deleteUser,
 } from "../controllers/userController.js";
 
+import { authenticate, restrict } from "../auth/verifyToken.js";
+
 const router = express.Router();
 
-router.get("/:id", getSingleUser);
-router.get("/", getAllUsers);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/:id", authenticate, restrict(["patient"]), getSingleUser);
+router.get("/", authenticate, restrict(["patient"]), getAllUsers);
+router.put("/:id", authenticate, restrict(["patient"]), updateUser);
+router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
 
 export default router;
